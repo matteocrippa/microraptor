aef2e3447d4ca6afb2d0842343e74ba1dc0f84c7
Matteo Crippa <matteocrippa@users.noreply.github.com>
2020-05-30 16:48:51 +0200

    update libs

--- test/microrequest_test.ts
+++ test/microrequest_test.ts
@@ -1,10 +1,12 @@
-import { assertEquals } from "https://deno.land/std@0.51.0/testing/asserts.ts";
-import { ServerRequest } from "https://deno.land/std@v0.51.0/http/server.ts";
+import { assertEquals } from "https://deno.land/std@0.54.0/testing/asserts.ts";
+import { ServerRequest } from "https://deno.land/std@0.54.0/http/server.ts";
 import { MicroRequest, Method } from "../lib/types/index.ts";
 
-Deno.test("Querystring", () => {
-  const dummyRequest = new ServerRequest();
-  dummyRequest.method = "GET";
+const dummyRequest = new ServerRequest();
+dummyRequest.headers = new Headers();
+dummyRequest.method = "GET";
+
+Deno.test("Querystring", async () => {
   dummyRequest.url = "/?string=value&number=2";
 
   const processed = new MicroRequest(dummyRequest, {
@@ -14,15 +16,14 @@ Deno.test("Querystring", () => {
       response: (req: MicroRequest) => {},
     },
   });
+  await processed.process();
 
   assertEquals(processed.query.string, "value");
   assertEquals(processed.query.number, "2");
   assertEquals(processed.query.notexisting, undefined);
 });
 
-Deno.test("Cookies", () => {
-  const dummyRequest = new ServerRequest();
-  dummyRequest.method = "GET";
+Deno.test("Cookies", async () => {
   dummyRequest.url = "/";
 
   const processed = new MicroRequest(dummyRequest, {
@@ -32,13 +33,12 @@ Deno.test("Cookies", () => {
       response: (req: MicroRequest) => {},
     },
   });
+  await processed.process();
 
   assertEquals(processed.cookie, {});
 });
 
-Deno.test("Params", () => {
-  const dummyRequest = new ServerRequest();
-  dummyRequest.method = "GET";
+Deno.test("Params", async () => {
   dummyRequest.url = "/second";
 
   const processed = new MicroRequest(dummyRequest, {
@@ -48,6 +48,8 @@ Deno.test("Params", () => {
       response: (req: MicroRequest) => {},
     },
   });
+
+  await processed.process();
 
   assertEquals(processed.param.first, "second");
 });
